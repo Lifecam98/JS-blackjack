d75b3dd959e69de55cb1a4b0639a30a266a8e3c5
Lifecam98 <tma092024@gmail.com>
2024-11-28 14:41:13 +0100

    10.4

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ restartButton.classList.add('disabled');
 
 function startGame() {
 	if (!gameStarted) {
-		firstCard = Math.floor(Math.random() * 12) + 2;
-		secondCard = Math.floor(Math.random() * 12) + 2;
+		firstCard = Math.floor(Math.random() * 13) + 2;
+		secondCard = Math.floor(Math.random() * 13) + 2;
 		cards.push(firstCard, secondCard);
 		gameStarted = true;
 		startGameButton.classList.add('disabled');
@@ -33,7 +33,12 @@ function startGame() {
 	renderGame();
 }
 
+// function renderCards() {
+
+// }
+
 function renderGame() {
+	// renderCards();
 	firstCard = firstCard > 11 ? 10: firstCard;
 	secondCard = secondCard > 11 ? 10: secondCard;
 	thirdCard = thirdCard > 11 ? 10 : thirdCard;
@@ -75,13 +80,13 @@ function renderGame() {
 
 function newCard() {
 	if (!thirdCard) {
-		thirdCard = Math.floor(Math.random() * 12) + 2;
+		thirdCard = Math.floor(Math.random() * 13) + 2;
 		cards.push(thirdCard);
 	} else if (!fourthCard) {
-		fourthCard = Math.floor(Math.random() * 12) + 2;
+		fourthCard = Math.floor(Math.random() * 13) + 2;
 		cards.push(fourthCard);
 	} else if (!fifthCard) {
-		fifthCard = Math.floor(Math.random() * 12) + 2;
+		fifthCard = Math.floor(Math.random() * 13) + 2;
 		cards.push(fifthCard);
 	}
 	renderGame();
